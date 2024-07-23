03314a971c40f867750c5275907654728ae84980
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-07-23 08:13:13 -0300

    V03: initial commit of the project

---
+++ anime-service/src/test/java/com/marcelomartinsdev/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelomartinsdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
