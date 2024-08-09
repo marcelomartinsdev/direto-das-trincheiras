49a5880502342602988b784024c6eca4be60f4ae
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-08-09 15:49:40 -0300

    Add getAnimes method in Anime.java, create PostMapping in AnimeController and Solve Exercise03.md

--- anime-service/src/main/java/com/marcelomartinsdev/controllers/AnimeController.java
+++ anime-service/src/main/java/com/marcelomartinsdev/controllers/AnimeController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("/v1/animes")
@@ -14,12 +15,12 @@ public class AnimeController {
 
     @GetMapping
     public List<Anime> listAll() {
-        return Anime.animeList();
+        return Anime.getAnimes();
     }
 
     @GetMapping("filter")
     public List<Anime> filter(@RequestParam(required = false) String name) {
-        var animes = Anime.animeList();
+        var animes = Anime.getAnimes();
         if (name == null || name.isEmpty()) {
             return animes;
         }
@@ -28,6 +29,13 @@ public class AnimeController {
 
     @GetMapping("{id}")
     public Anime findById(@PathVariable Long id) {
-        return Anime.animeList().stream().filter(anime -> anime.getId().equals(id)).findFirst().orElse(null);
+        return Anime.getAnimes().stream().filter(anime -> anime.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @PostMapping
+    public Anime save(@RequestBody Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(100_000));
+        Anime.getAnimes().add(anime);
+        return anime;
     }
 }
--- anime-service/src/main/java/com/marcelomartinsdev/domain/Anime.java
+++ anime-service/src/main/java/com/marcelomartinsdev/domain/Anime.java
@@ -2,21 +2,30 @@ package com.marcelomartinsdev.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class Anime {
+    private static List<Anime> animes = new ArrayList<Anime>();
+
+    static {
+        var onePiece = new Anime("One Piece", 1L);
+        var naruto = new Anime("Naruto", 2L);
+        var dragonBallZ = new Anime("Dragon Ball Z", 3L);
+
+        animes.addAll(List.of(onePiece, naruto, dragonBallZ));
+    }
+
     public String name;
     private Long id;
 
-    public static List<Anime> animeList() {
-        var onePiece = new Anime("One Piece", 1L);
-        var moshukoTensei = new Anime("Moshuko Tensei", 2L);
-        var naruto = new Anime("Naruto", 3L);
-
-        return List.of(onePiece, moshukoTensei, naruto);
+    public static List<Anime> getAnimes() {
+        return animes;
     }
 }
 
